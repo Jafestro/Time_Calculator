b395c78add09ab78e37a6bebe8e13725f9d3210d
jafestro <jafestro@gmail.com>
2024-09-23 23:18:57 +0300

    feat: Add Dockerfile

--- src/main/java/com/jafestro/TimeCalculator.java
+++ src/main/java/com/jafestro/TimeCalculator.java
@@ -15,4 +15,12 @@ public class TimeCalculator {
         double time = distance / speed; // in hours
         return time;
     }
+
+    public static void main(String[] args) {
+        TimeCalculator calculator = new TimeCalculator();
+        System.out.println("Hello from Docker! It is Jafar here.");
+        System.out.println("Distance: 100.0 km, Speed: 50.0 km/h");
+        System.out.print("Time: ");
+        System.out.println(calculator.calculateTime(100.0, 50.0));
+    }
 }
